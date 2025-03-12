9c61b9c7eb4c46c4d7ef37fa31b3daf6e776dcc0
nurzulaikha <nurzulaikha@iium.edu.my>
2025-03-12 15:29:38 +0800

    Initial commit

---
+++ AdminAsset.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace ekalokman\AdminOci8;
+
+/**
+ * AdminAsset
+ *
+ * @author Apaoww <apao.oww@gmail.com>
+ * @since 1.0
+ */
+class AdminAsset extends \yii\web\AssetBundle
+{
+    /**
+     * @inheritdoc
+     */
+    public $sourcePath = '@ekalokman/AdminOci8';
+
+    /**
+     * @inheritdoc
+     */
+    public $css = [
+        'main.css',
+    ];
+
+}
